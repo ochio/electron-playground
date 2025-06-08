dc298d86baaa2cec10814851cff63f0c9655aa89
ochi <ocos54ce@gmail.com>
2025-06-08 18:10:47 +0900

    input url

--- index.js
+++ index.js
@@ -1,19 +1,58 @@
-const { app, BrowserWindow, ipcMain } = require('electron')
-const path = require('node:path')
+// main.js
+const { app, ipcMain, BrowserWindow, WebContentsView, screen } = require('electron');
+const path = require('node:path');
 
-function createWindow () {
-  const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+let leftView, rightView;
 
-  mainWindow.loadFile('index.html')
+// BrowserWindow 作成後にショートカットを登録
+function registerShortcuts() {
+  // CommandOrControl+L で Renderer へイベント送信
+  globalShortcut.register('CommandOrControl+L', () => {
+    // フォーカスが別ビューにあっても必ず飛ばす
+    win.webContents.send('focus-url-input');
+  });
 }
 
-app.whenReady().then(() => {
-  ipcMain.handle('ping', () => 'pong')
-  createWindow()
-})  
+
+function createSplitWindow() {
+  const { workArea } = screen.getPrimaryDisplay();
+  const win = new BrowserWindow({
+    x: workArea.x,
+    y: workArea.y,
+    width: workArea.width,
+    height: workArea.height,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true,
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  win.loadFile(path.join(__dirname, 'index.html'));
+
+  leftView  = new WebContentsView({ webPreferences: { nodeIntegration:false, contextIsolation:true } });
+  rightView = new WebContentsView({ webPreferences: { nodeIntegration:false, contextIsolation:true } });
+  win.contentView.addChildView(leftView);
+  win.contentView.addChildView(rightView);
+
+  const half = Math.floor(workArea.width / 2);
+  leftView.setBounds({  x: 0,     y: 200, width: half, height: workArea.height });
+  rightView.setBounds({ x: half,  y: 200, width: half, height: workArea.height });
+
+  leftView.setAutoResize({ width: true, height: true });
+  rightView.setAutoResize({ width: true, height: true });
+
+  registerShortcuts();
+}
+
+ipcMain.handle('load-url', (_evt, rawUrl) => {
+  // プレフィクス補完（例: example.com → https://example.com）
+  const url =
+    /^(https?|file):\/\//i.test(rawUrl) ? rawUrl : `https://${rawUrl}`;
+  const translatedUrl = `https://translate.google.com/translate?hl=ja&sl=auto&tl=ja&u=${url}`
+
+  leftView?.webContents.loadURL(translatedUrl);
+  rightView?.webContents.loadURL(url);
+});
+
+app.whenReady().then(createSplitWindow);
--- preload.js
+++ preload.js
@@ -1,8 +1,6 @@
-const { contextBridge, ipcRenderer } = require('electron')
+const { contextBridge, ipcRenderer } = require('electron');
 
-contextBridge.exposeInMainWorld('versions', {
-  node: () => process.versions.node,
-  chrome: () => process.versions.chrome,
-  electron: () => process.versions.electron,
-  ping: () => ipcRenderer.invoke('ping')
-})
+contextBridge.exposeInMainWorld('electronAPI', {
+  loadUrl:  (url) => ipcRenderer.invoke('load-url', url),
+  onFocusUrlInput: (fn) => ipcRenderer.on('focus-url-input', fn),
+});
