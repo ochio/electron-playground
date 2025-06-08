e9f784d551a971a87e2f07114c26272c4c94a854
ochi <ocos54ce@gmail.com>
2025-06-08 16:56:28 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require('node:path')
+
+function createWindow () {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  mainWindow.loadFile('index.html')
+}
+
+app.whenReady().then(() => {
+  ipcMain.handle('ping', () => 'pong')
+  createWindow()
+})  
